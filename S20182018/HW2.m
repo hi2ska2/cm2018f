3d8c3d9bde0dfa82e3a3e8c9beb7220cdc40ff47
wbju <42921031+wbju@users.noreply.github.com>
2018-09-07 00:49:26 +0900

    Create HW2.m
    E(N=5) = 0.0753 eV
    E(N=50) = 0.0792 eV
    E(N=500) = 0.0793 eV

---
+++ S20182018/HW2.m
@@ -0,0 +1,21 @@
+function [E] = HW2(N)
+
+m0=0.511*10^(6)/(3*10^8)^2; % Electron rest mass 
+h=6.582*10^(-16); % h-bar [eV*s]
+a=5*10^(-9); % Well width
+dx=a/(N-1); % Delta x
+
+A=[];
+A(1,1)=-2;
+A(1,2)=1;
+for n=2:N-3
+    A(n,n+1)=1;
+    A(n,n)=-2;
+    A(n,n-1)=1;
+end
+A(N-2,N-3)=1;
+A(N-2,N-2)=-2;
+
+e = eig(A);
+ksquare=-e(N-2)/(dx)^2;
+E=h*h*ksquare/2/m0/0.19; % Unit: eV
